fa70a65349537abe3b69d9e1af1a325149c8e27d
amandeeprikhi <amandeeprikhi@gmail.com>
2018-07-12 17:32:18 +0530

    Added the show page

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -8,16 +8,24 @@ class RecipesTest < ActionDispatch::IntegrationTest
     @recipe2 = Recipe.create(name: "Chicken Saute", description: "Great chicken dish", chef: @chef)
   end
 
-  test "should get recipes index" do
-    get recipes_path
-    assert_response :success
-  end
+  # test "should get recipes index" do
+  #   get recipes_path
+  #   assert_response :success
+  # end
+  #
+  # test "should get recipes listing" do
+  #   get recipes_path
+  #   assert_template 'recipes/index'
+  #   assert_match @recipe.name, response.body
+  #   assert_match @recipe2.name, response.body
+  # end
 
-  test "should get recipes listing" do
-    get recipes_path
-    assert_template 'recipes/index'
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
     assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
   end
 
 end
